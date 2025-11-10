cdcec87d21caa6c47f63c58bd6dff79bb8344a90
ShrutiJangale <shruti.j@ergobite.com>
2025-11-10 15:57:31 +0530

    Initial commit: royalfinanceAI Django app

--- bankstatement_project/urls.py
+++ bankstatement_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('analyzer/', include('statement_analyzer.urls')), # Include your app's URLs
+    path('', RedirectView.as_view(url='/analyzer/upload/', permanent=False)),
 ]
